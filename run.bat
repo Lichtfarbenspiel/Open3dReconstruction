17bc194a267104fe2c69de4462bb977af3ef7a34
Lichtfarbenspiel <s.belua.d@gmail.com>
2022-06-20 19:52:21 +0200

    run.bat added/updated

--- run.bat
+++ run.bat
@@ -2,10 +2,10 @@
 python azure_kinect_mkv_reader.py --input kinect_recording.mkv --output frames
 
 :: Reconstruction pipeline
-python run_system.py "config.json" --make
+python run_system.py "%1" --make
 
-python run_system.py "config.json" --register
+python run_system.py "%1" --register
 
-python run_system.py "config.json" --refine
+python run_system.py "%1" --refine
 
-python run_system.py "config.json" --integrate
+python run_system.py "%1" --integrate
